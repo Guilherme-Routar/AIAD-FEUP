9b58818074e65a1ac175d16b7890301162d29c28
Guilherme Routar <groutar@gmail.com>
2016-11-14 23:37:06 +0000

    Visualization prototype implemented - visible sensors

--- src/Agents/SensorAgent.java
+++ src/Agents/SensorAgent.java
@@ -1,9 +1,45 @@
 package Agents;
 
+import java.awt.Color;
+
+import SIMLauncher.SIMLauncher;
+import jade.core.behaviours.CyclicBehaviour;
 import sajas.core.Agent;
 
-public class SensorAgent extends Agent{
+import uchicago.src.sim.gui.Drawable;
+import uchicago.src.sim.gui.SimGraphics;
+import uchicago.src.sim.space.Object2DGrid;
 
-	public SensorAgent() {}
-	public void setup() {}
+public class SensorAgent extends Agent implements Drawable{
+	
+	private int x;
+	private int y;
+	private Object2DGrid grid;
+	private Color color;
+
+	public SensorAgent(int x, int y, Object2DGrid grid, Color color) {
+		this.x = x;
+		this.y = y;
+		this.grid = grid;
+		this.color = color;
+	}
+	
+	public void setup() {
+		grid.putObjectAt(this.x, this.y, this);
+	}
+
+	@Override
+	public void draw(SimGraphics sim) {
+		sim.drawFastRect(color);
+	}
+
+	@Override
+	public int getX() {
+		return x;
+	}
+
+	@Override
+	public int getY() {
+		return y;
+	}
 }
--- src/SIMLauncher/SIMLauncher.java
+++ src/SIMLauncher/SIMLauncher.java
@@ -1,4 +1,7 @@
 package SIMLauncher;
+import java.awt.Color;
+import java.util.ArrayList;
+
 import Agents.SensorAgent;
 import jade.core.Profile;
 import jade.core.ProfileImpl;
@@ -8,24 +11,49 @@ import sajas.core.Agent;
 import sajas.core.Runtime;
 import sajas.sim.repast3.Repast3Launcher;
 import sajas.wrapper.ContainerController;
-
+import uchicago.src.sim.analysis.OpenSequenceGraph;
+import uchicago.src.sim.analysis.Sequence;
+import uchicago.src.sim.engine.Schedule;
 import uchicago.src.sim.engine.SimInit;
+import uchicago.src.sim.gui.DisplaySurface;
+import uchicago.src.sim.gui.Object2DDisplay;
+import uchicago.src.sim.space.Object2DGrid;
+import uchicago.src.sim.util.Random;
 
 public class SIMLauncher extends Repast3Launcher {
 	
 	//Parameters
 	private ContainerController mainContainer;
-	private int numOfSensors = 10;
+	
+	private ArrayList<SensorAgent> sensorsList;
+	
+	private Object2DGrid grid;
+	private DisplaySurface surface;
+	private OpenSequenceGraph plot;
+	private int numOfSensors, gridSize;
 	//-----
 	
-	@Override
-	public String[] getInitParam() {
-		return new String[0];
+	public SIMLauncher() {
+		this.numOfSensors = 10;
+		this.gridSize = 100;
 	}
-
+	
 	@Override
 	public String getName() {
 		return "WSN Optimization";
+	}
+	
+	@Override
+	public String[] getInitParam() {
+		return new String[] { "numOfSensors", "gridSize" };
+	}
+	
+	@Override
+	public void setup() {
+		super.setup();
+		
+		surface = new DisplaySurface(this, "This is a display");
+		registerDisplaySurface("This is a display", surface);
 	}
 
 	@Override
@@ -41,27 +69,74 @@ public class SIMLauncher extends Repast3Launcher {
 	private void launchAgents() {
 		
 		try {
-			
 			int i;
-			for (i = 0; i < numOfSensors; i++) {
-				SensorAgent agent = new SensorAgent();
-				mainContainer.acceptNewAgent("S" + (i + 1), agent).start();
-			}
+			for (i = 0; i < numOfSensors; i++) 
+				mainContainer.acceptNewAgent("S" + (i + 1), createAgent()).start();
 		} catch (StaleProxyException e) {
 			e.printStackTrace();
 		}
-		
-	}
-
-	@Override
-	public void setup() {
-		super.setup();
 	}
 
 	//Runs after playing simulation
 	@Override
 	public void begin() {
-		super.begin();		
+		//System.out.println(getInitParam()[1]);
+		buildModel();
+		buildDisplay();
+		super.begin();	
+		buildSchedule();
+	}
+	
+	//Create and store agents; Create space, data recorders
+	private void buildModel() {
+		sensorsList = new ArrayList<SensorAgent>();
+		grid = new Object2DGrid(gridSize, gridSize);
+	}
+	
+	SensorAgent createAgent() {
+		
+		int x, y;
+		do {
+			x = Random.uniform.nextIntFromTo(0, grid.getSizeX() - 1);
+			y = Random.uniform.nextIntFromTo(0, grid.getSizeY() - 1);
+		} while (grid.getObjectAt(x, y) != null);
+			
+		Color color = new Color(Random.uniform.nextIntFromTo(0, 255), Random.uniform.nextIntFromTo(0, 255),
+				Random.uniform.nextIntFromTo(0, 255));
+		
+		SensorAgent sensor = new SensorAgent(x, y, grid, color);
+		grid.putObjectAt(x, y, sensor);
+		sensorsList.add(sensor);
+		
+		return sensor;
+	}
+	
+	//Create displays, charts
+	private void buildDisplay() {
+		Object2DDisplay sensorsDisplay = new Object2DDisplay(grid);
+		sensorsDisplay.setObjectList(sensorsList);
+		
+		surface.addDisplayableProbeable(sensorsDisplay, "Sensors");
+		addSimEventListener(surface);
+		surface.display();
+		
+		// graph
+		plot = new OpenSequenceGraph("Number of Sensors", this);
+		plot.setAxisTitles("time", "n");
+
+		// plot number of different existing colors
+		plot.addSequence("Number of sensors", new Sequence() {
+			public double getSValue() {
+				return sensorsList.size();
+				}
+			});
+		plot.display();
+	}
+	
+	//Build the schedule
+	private void buildSchedule() {
+		getSchedule().scheduleActionAtInterval(1, surface, "updateDisplay", Schedule.LAST);
+		getSchedule().scheduleActionAtInterval(1, plot, "step", Schedule.LAST);
 	}
 
 	/**
