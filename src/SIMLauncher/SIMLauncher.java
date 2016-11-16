db8e92b90b2f405c35f0cf5bf523fa3f63bc9696
Guilherme Routar <groutar@gmail.com>
2016-11-16 15:57:34 +0000

    Added different scenarios

--- src/SIMLauncher/SIMLauncher.java
+++ src/SIMLauncher/SIMLauncher.java
@@ -22,75 +22,81 @@ public class SIMLauncher extends Repast3Launcher {
 
 	//Parameters
 	private ContainerController mainContainer;
-	private ArrayList<Object> riverCells; 
+	private ArrayList<Water> waterCells; 
+	private ArrayList<Sensor> sensors;
 	//Simulation elements
 	private Object2DGrid river;
 	private DisplaySurface surface;
 	private OpenSequenceGraph plot;
 	//Values
-	private int NUM_SENSORS = 10;
-	private int RIVER_WIDTH = 100;
-	private int RIVER_HEIGHT = 20;
+	private int NUM_SENSORS;
+	private static int CELLS_PER_KM = 5;
+	private int RIVER_WIDTH = 50 * CELLS_PER_KM;
+	private int RIVER_HEIGHT = 2 * CELLS_PER_KM;
 	private static boolean BATCH_MODE = true;
+	//Scenarios (Sensors allocation along the river)
+	private static boolean CHAINALONGRIVER = true; //Allocated in a line throughout the river
+	private static boolean ENDOFRIVER = false; //Alocated in collumns of 3 sensors by the end of the river
+	private static boolean RANDOM = false; //Alocated in random positions of the river
 	//-----
 
 	public SIMLauncher() { 
 		super(); 
 	}
-	
+
 	//Get and Set functions -----
 	public int getNUM_SENSORS() {
 		return NUM_SENSORS;
 	}
-	
+
 	public void setNUM_SENSORS(int NUM_SENSORS) {
 		this.NUM_SENSORS = NUM_SENSORS;
 	}
-	
+
 	public int getRIVER_WIDTH() {
 		return RIVER_WIDTH;
 	}
-	
+
 	public void setRIVER_WIDTH(int RIVER_WIDTH) {
 		this.RIVER_WIDTH = RIVER_WIDTH;
 	}
-	
+
 	public int getRIVER_HEIGHT() {
 		return RIVER_HEIGHT;
 	}
-	
+
 	public void setRIVER_HEIGHT(int RIVER_HEIGHT) {
 		this.RIVER_HEIGHT = RIVER_HEIGHT;
 	}
 	//-----
-	
+
 	@Override
 	public String[] getInitParam() {
 		return new String[] {"NUM_SENSORS", "RIVER_WIDTH", "RIVER_HEIGHT"};
 	}	
-	
+
 	@Override
 	public String getName() {
 		return "WSN Optimization";
 	}
-	
+
 	@Override
 	public void setup() {
 		super.setup();
-		
+
 		if (surface != null) surface.dispose();
 
 		surface = new DisplaySurface(this, "Sensors river");
 		registerDisplaySurface("Sensors river", surface);
 	}
-	
+
 	@Override
 	protected void launchJADE() {
 
 		Runtime rt = Runtime.instance();
 		Profile p1 = new ProfileImpl();
 		mainContainer = rt.createMainContainer(p1);
-		
+
 		launchWater();
 		launchSensors();
 	}
@@ -98,38 +104,65 @@ public class SIMLauncher extends Repast3Launcher {
 	private void launchSensors() {
 
 		try {
-			int i;
-			for (i = 0; i < NUM_SENSORS; i++) 
-				mainContainer.acceptNewAgent("S" + (i + 1), createSensor(i)).start();
-		} catch (StaleProxyException e) {
+			Sensor sensor;
+			int y, x;
+			if (CHAINALONGRIVER) {
+				setNUM_SENSORS(50);
+				for (int i = 0; i < NUM_SENSORS; i++) {
+					x = i * (river.getSizeX() / getNUM_SENSORS());
+					y = river.getSizeY() / 2;
+					sensor = allocateSensor(x, y);
+					mainContainer.acceptNewAgent("S-" + i, sensor).start();
+				}
+			}
+			else if (ENDOFRIVER) {
+				setNUM_SENSORS(30);
+				for (int i = 0; i < NUM_SENSORS / 3; i++) {
+					for (int j = 0; j < 3; j++) {
+						x = (i * (river.getSizeX() / getNUM_SENSORS())) / 4;
+						y = (j * 3) + 2;
+						sensor = allocateSensor(x, y);
+						mainContainer.acceptNewAgent("S-" + (i * 3 + j), sensor).start();
+					}
+				}
+			}
+			else if (RANDOM) {
+				setNUM_SENSORS(50);
+				for (int i = 0; i < NUM_SENSORS; i++) {
+					x = Random.uniform.nextIntFromTo(0, river.getSizeX() - 1);
+					y = Random.uniform.nextIntFromTo(0, river.getSizeY() - 1);
+					sensor = allocateSensor(x, y);
+					mainContainer.acceptNewAgent("S-" + i, sensor).start();
+				}
+			}
+		}
+		catch (StaleProxyException e) {
 			e.printStackTrace();
 		}
 	}
+	
+	private Sensor allocateSensor(int x, int y) {
 
-	Sensor createSensor(int pos) {
-
-		int x, y;
-		x = pos * (river.getSizeX() / getNUM_SENSORS());
-		y = river.getSizeY() / 2;
-		
+		/*
+		//Random color for each sensor
 		Color color = new Color(Random.uniform.nextIntFromTo(0, 255), 
 				Random.uniform.nextIntFromTo(0, 255), 
 				Random.uniform.nextIntFromTo(0, 255));
+		*/
 		
-		Sensor sensor = new Sensor(x, y, river, color);
+		Sensor sensor = new Sensor(x, y, river, Color.BLACK);
 		river.putObjectAt(x, y, sensor);
-		riverCells.add(sensor);
-
+		sensors.add(sensor);
 		return sensor;
 	}
 	
 	private void launchWater() {
-		
+
 		for (int i = 0; i < RIVER_WIDTH; i++) {
 			for (int j = 0; j < RIVER_HEIGHT; j++) {
 				Water waterCell = new Water(i,j);
 				river.putObjectAt(i, j, waterCell);
-				riverCells.add(waterCell);
+				waterCells.add(waterCell);
 			}
 		}
 	}
@@ -145,39 +178,47 @@ public class SIMLauncher extends Repast3Launcher {
 
 	//Create and store agents; Create space, data recorders
 	private void buildModel() {
-		riverCells = new ArrayList<Object>();
+		waterCells = new ArrayList<Water>();
+		sensors = new ArrayList<Sensor>();
 		river = new Object2DGrid(RIVER_WIDTH, RIVER_HEIGHT);
 	}
 
 	//Create displays, charts
 	private void buildDisplay() {
 
-		//Grid 
+		Object2DDisplay waterDisplay = new Object2DDisplay(river);
+		waterDisplay.setObjectList(waterCells);
+		surface.addDisplayableProbeable(waterDisplay, "River");
+		addSimEventListener(surface);
+
 		Object2DDisplay sensorsDisplay = new Object2DDisplay(river);
-		sensorsDisplay.setObjectList(riverCells);
+		sensorsDisplay.setObjectList(sensors);
 		surface.addDisplayableProbeable(sensorsDisplay, "Sensors");
 		addSimEventListener(surface);
+
 		surface.display();
 
 		//Graph
-		plot = new OpenSequenceGraph("Number of Sensors", this);
+		/*
+		plot = new OpenSequenceGraph("Number of River Cells", this);
 		plot.setAxisTitles("time", "n");
 
 		// plot number of different existing colors
-		plot.addSequence("Number of sensors", new Sequence() {
+		plot.addSequence("River Cells", new Sequence() {
 			public double getSValue() {
 				return riverCells.size();
 			}
 		});
 		plot.display();
+		 */
 	}
 
 	//Build the schedule
 	private void buildSchedule() {
 		getSchedule().scheduleActionAtInterval(1, surface, "updateDisplay", Schedule.LAST);
-		getSchedule().scheduleActionAtInterval(1, plot, "step", Schedule.LAST);
+		//getSchedule().scheduleActionAtInterval(1, plot, "step", Schedule.LAST);
 	}
-	
+
 	//Launching Repast3
 	public static void main(String[] args) {
 		SimInit init = new SimInit();
